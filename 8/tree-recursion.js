d41e66a2233f4ae019421b6519d885f441313c41
Matt <matt.lee.personal@gmail.com>
2018-12-10 18:43:15 -0600

    Day 8 solution

---
+++ 8/tree-recursion.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const fsPromises = fs.promises
+
+fsPromises.open('input.txt', 'r')
+  .then(fh => {
+    return fh.readFile({ encoding: 'utf-8', flag: 'r' });
+  })
+  .then(data => {
+    data = data.split(' ').map(char => Number(char));
+    let result = sumMetadata(data);
+    console.log('Metadata sum: ', result);
+  })
+  .catch(err => console.log(err))
+
+  function sumMetadata(inputTree) {
+    let metaSum = 0;
+
+    function treeRecurse(tree) {
+      while(tree.length > 0) {
+        let childNodes = tree.shift();
+        let metaLength = tree.shift();
+  
+        // base
+        if(childNodes === 0) {
+          tree.splice(0, metaLength).forEach(el => metaSum += el);
+          return;
+        } else {
+          // recurse
+          for(let i = 0; i < childNodes; i++) {
+            treeRecurse(tree);
+          }
+
+          // base
+          tree.splice(0, metaLength).forEach(el => metaSum += el);
+          return;
+        }
+      }
+    }
+
+    treeRecurse(inputTree);
+    return metaSum;
+  }
